4657e11af3528720a50590f50a0a9f0c0f6a716f
OlgaNoz <onozdrya@yandex.ru>
2023-04-06 16:11:22 +0300

    fixed tests

--- server.js
+++ server.js
@@ -5,6 +5,9 @@ const PORT = 3000;
 
 app.use(express.static('./dist'));
 app.use(express.static('./static/icons'));
+app.use('/*', (req, res) => {
+  res.sendFile(path.join(__dirname, './dist/index.html'));
+});
 
 app.listen(PORT, function () {
   console.log(`Example app listening on port ${PORT}!`);
