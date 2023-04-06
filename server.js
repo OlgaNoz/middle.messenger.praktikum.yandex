826ec9e469ce6132bd91348119316fe1cabc1eaa
OlgaNoz <onozdrya@yandex.ru>
2023-04-06 16:08:32 +0300

    fixed build

--- server.js
+++ server.js
@@ -3,7 +3,6 @@ const express = require('express');
 const app = express();
 const PORT = 3000;
 
-console.log(__dirname);
 app.use(express.static('./dist'));
 app.use(express.static('./static/icons'));
 
